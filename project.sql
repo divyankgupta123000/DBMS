213288b8d349556f0a425097a3d283b97af6082c
divyankgupta123000 <53806810+divyankgupta123000@users.noreply.github.com>
2020-05-09 18:49:30 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -2,12 +2,12 @@ create database project;
 use project;
 
 create table Customer(
-cust_id int,
+cust_id int unique not null,
 cname varchar(30) not null,
 csex varchar(10) not null,
 cage int not null,
-cphone int not null unique,
-primary key(cust_id));
+cphone,
+primary key(cphone));
 
 create table Discount(
 ctype varchar(30),
@@ -17,23 +17,23 @@ primary key(ctype));
 
 create table Card(
 card_id int,
-cust_id int not null,
+cphone int not null,
 ctype varchar(30) not null,
 expdate date,
 primary key(card_id), 
-foreign key(cust_id) references Customer(cust_id),
+foreign key(cphone) references Customer(cphone),
 foreign key(ctype) references Discount(ctype));
 
 create table Receipt(
 rnumber int,
-cust_id int not null,
+cphone int not null,
 t_date date,
 total_items int not null,
 total_amt int not null,
 disc_amt int,
 card_id int,
 primary key(rnumber),
-foreign key(cust_id) references Customer(cust_id),
+foreign key(cphone) references Customer(cphone),
 foreign key(card_id) references Card(card_id));
 
 create table PayMode(
